2bdb923a2b31c5422319ccaf7ef29b0ee5076e48
Thijs Koerselman <thijskoerselman@gmail.com>
2023-07-16 12:34:51 +0200

    Use shrinkwrap in detecting npm

--- src/helpers/detect-package-manager.ts
+++ src/helpers/detect-package-manager.ts
@@ -76,6 +76,13 @@ function inferFromFiles(workspaceRoot: string): PackageManager {
     }
   }
 
+  /**
+   * If no lockfile was found, it could be that there is an npm shrinkwrap file.
+   */
+  if (fs.existsSync(path.join(workspaceRoot, "npm-shrinkwrap.json"))) {
+    return { name: "npm", version: getVersion("npm") };
+  }
+
   throw new Error(`Failed to detect package manager`);
 }
 
