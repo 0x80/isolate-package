406552ba063f99753d65eb0c108b9dd2a7d69621
jieey1140 <seihoonkim@hansei.kr>
2023-08-15 23:42:10 +0900

    fix: exec options name

--- src/utils/pack.ts
+++ src/utils/pack.ts
@@ -9,7 +9,7 @@ export async function pack(
   dstDir: string,
   usePnpmPack = false
 ) {
-  const execOption = {
+  const execOptions = {
     maxBuffer: 10 * 1024 * 1024,
   };
 
@@ -26,7 +26,7 @@ export async function pack(
     ? await new Promise<string>((resolve, reject) => {
         exec(
           `pnpm pack --pack-destination ${dstDir}`,
-          execOption,
+          execOptions,
           (err, stdout, stderr) => {
             if (err) {
               log.error(stderr);
@@ -40,7 +40,7 @@ export async function pack(
     : await new Promise<string>((resolve, reject) => {
         exec(
           `npm pack --pack-destination ${dstDir}`,
-          execOption,
+          execOptions,
           (err, stdout) => {
             if (err) {
               return reject(err);
