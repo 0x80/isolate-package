03e9fc55132b619ea7bdacf931b9d2ffdfc2409c
Thijs Koerselman <thijskoerselman@gmail.com>
2023-11-27 14:29:29 +0100

    Export a type for the exported function

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
-export * from "./isolate";
+import type { isolate } from "./isolate";
+export { isolate } from "./isolate";
 export type { Logger } from "./utils/logger";
+
+export type IsolateExports = {
+  isolate: typeof isolate;
+};
