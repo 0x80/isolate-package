d81b40b02810320d17ab63d95d3a2e3b0e04629a
Thijs Koerselman <thijskoerselman@gmail.com>
2023-11-27 12:01:37 +0100

    Only output esm

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,22 +1,17 @@
 import { defineConfig } from "tsup";
-/**
- * TSUP doesn't seem to generate js files for cjs anymore if we map js to mjs,
- * so we need to use two different configs and build twice.
- *
- * ESM is built first, so we can use clean: true here.
- */
+
 export default defineConfig({
   entry: {
     index: "src/index.ts",
     "isolate-bin": "src/isolate-bin.ts",
   },
-  format: ["esm", "cjs"],
+  format: ["esm"],
   target: "node18",
   sourcemap: true,
   splitting: false,
   dts: true,
   clean: true,
-  shims: true, // replaces use of import.meta
+  // shims: true, // replaces use of import.meta
   /**
    * The `isolate` binary is an ES module. The file is required to have the
    * `.mjs` file extension, otherwise a non-ESM workspace will try to execute it
@@ -24,12 +19,10 @@ export default defineConfig({
    *
    * For details see [this article from Alex
    * Rauschmayer](https://exploringjs.com/nodejs-shell-scripting/ch_creating-shell-scripts.html
-   *
-   * Js-esm-modules-as-standalone-shell-scripts-on-unix)
    */
-  // outExtension() {
-  //   return {
-  //     js: `.mjs`,
-  //   };
-  // },
+  outExtension() {
+    return {
+      js: `.mjs`,
+    };
+  },
 });
