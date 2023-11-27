934b3c11fd7023ba7dafefb405f3897b6a0f93e9
Thijs Koerselman <thijskoerselman@gmail.com>
2023-11-27 12:01:27 +0100

    Add test for loading esm in cjs

--- test-load-with-require/index.ts
+++ test-load-with-require/index.ts
@@ -1,16 +1,16 @@
 /**
- * Test importing from a CJS context.
- *
- * This script will result in an error if normal required is used, because ESM
- * dependencies are not transpiled to CJS by a bundler.
+ * This script will result in an error if ESM dependencies were not transpiled
+ * to CJS.
  *
  * Error [ERR_REQUIRE_ESM]: require() of ES Module [module] is not supported.
  * Instead change the require of [filename] in [directory] to a dynamic import()
  * which is available in all CommonJS modules.
  */
+
 (async () => {
   const { isolate } = await import("isolate-package");
 
+  //  / console.log(isolate);
   isolate()
     .then((isolateDir) => {
       console.log("Isolate created at", isolateDir);
