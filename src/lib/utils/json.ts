ecd17b4783accc115c55c4bbe2dd2e6bdf9e168c
Vajahath Ahmed <12165822+vajahath@users.noreply.github.com>
2024-03-28 21:12:11 +0530

    Formatting changes (#60)

--- src/lib/utils/json.ts
+++ src/lib/utils/json.ts
@@ -6,7 +6,9 @@ import { getErrorMessage } from "./get-error-message";
 export function readTypedJsonSync<T>(filePath: string) {
   try {
     const rawContent = fs.readFileSync(filePath, "utf-8");
-    const data = JSON.parse(stripJsonComments(rawContent, { trailingCommas: true })) as T;
+    const data = JSON.parse(
+      stripJsonComments(rawContent, { trailingCommas: true })
+    ) as T;
     return data;
   } catch (err) {
     throw new Error(
@@ -18,7 +20,9 @@ export function readTypedJsonSync<T>(filePath: string) {
 export async function readTypedJson<T>(filePath: string) {
   try {
     const rawContent = await fs.readFile(filePath, "utf-8");
-    const data = JSON.parse(stripJsonComments(rawContent, { trailingCommas: true })) as T;
+    const data = JSON.parse(
+      stripJsonComments(rawContent, { trailingCommas: true })
+    ) as T;
     return data;
   } catch (err) {
     throw new Error(
