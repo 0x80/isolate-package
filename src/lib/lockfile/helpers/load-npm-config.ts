24811faba3cf11299823929361a5568d92ba3370
Laurence Hartgill <laurence@hartgill.co.uk>
2024-10-23 09:28:41 +0100

    Improve npm config load readability

--- src/lib/lockfile/helpers/load-npm-config.ts
+++ src/lib/lockfile/helpers/load-npm-config.ts
@@ -2,14 +2,14 @@ import Config from "@npmcli/config";
 import defaults from "@npmcli/config/lib/definitions/index.js";
 
 export async function loadNpmConfig({ npmPath }: { npmPath: string }) {
-  const conf = new Config({
+  const config = new Config({
     npmPath,
     definitions: defaults.definitions,
     shorthands: defaults.shorthands,
     flatten: defaults.flatten,
   });
 
-  await conf.load();
+  await config.load();
 
-  return conf;
+  return config;
 }
